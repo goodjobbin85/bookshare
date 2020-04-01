a258d6aa06a15b87d2bddb2de18afbcb66ec5109
thomas dubiel <github email address>
2020-04-01 15:55:02 -0500

    index, show, new CRUD functionality, views and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,32 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    @users = User.all
+    erb :'/users/index'
+  end
+
+  get '/users' do
+    @users = User.all
+    erb :'/users/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :'users/show'
+  end
+
+
+  post '/users' do
+    @user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    if @user
+      redirect to :"/users/#{@user.id}"
+    else
+      erb :'failure'
+    end
   end
 
 end
