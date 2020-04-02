826f82c325810bfb537ce3fe7c33bd5184ce382e
thomas dubiel <github email address>
2020-04-02 11:47:14 -0500

    create book show view and controller action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,9 @@ class BooksController < Sinatra::Base
     @books = Book.all
     erb :'books/index'
   end
+
+  get '/books/:id' do
+    @book = Book.find(params[:id])
+    erb :'books/show'
+  end
 end
