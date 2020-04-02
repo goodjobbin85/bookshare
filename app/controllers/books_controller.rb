d989c755c64621220b7330efb9c589246b51a935
thomas dubiel <github email address>
2020-04-02 12:35:38 -0500

    create new action in controller and new view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,8 +12,24 @@ class BooksController < Sinatra::Base
     erb :'books/index'
   end
 
+  get '/books/new' do
+    erb :'books/new'
+  end
+
   get '/books/:id' do
     @book = Book.find(params[:id])
     erb :'books/show'
   end
+
+  post '/books' do
+    @book = Book.new
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.year_published = params[:year_published]
+    @book.condition = params[:condition]
+    @book.save
+    redirect to "/books/#{@book.id}"
+  end
+
+
 end
